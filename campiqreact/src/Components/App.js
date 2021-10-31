9c5ae4c16183467a245c4e5d6e158f7f856dd95e
ablodgett <alexjblodgett@gmail.com>
2021-10-31 12:02:30 -0400

    Added styles and feed + list to home component

--- campiqreact/src/Components/App.js
+++ campiqreact/src/Components/App.js
@@ -7,10 +7,28 @@ import Location from "./location";
 import AddReview from "./addreview";
 import PackListComponent from "./packlist";
 import { MuiThemeProvider } from "@material-ui/core/styles";
-import { Toolbar, AppBar, Typography, Menu, MenuItem, IconButton } from "@material-ui/core";
-import theme from "../theme";
+import {
+  Toolbar,
+  AppBar,
+  Typography,
+  Menu,
+  MenuItem,
+  IconButton,
+} from "@material-ui/core";
+import theme from "assets/theme/theme.js";
 import logo from "./campIQLogo.jpg";
 import AccountBenefits from "./accountbenefits";
+
+// plugins styles from node_modules
+import "react-perfect-scrollbar/dist/css/styles.css";
+import "@fullcalendar/common/main.min.css";
+import "@fullcalendar/daygrid/main.min.css";
+import "quill/dist/quill.core.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+// plugins styles downloaded
+import "assets/plugins/nucleo/css/nucleo.css";
+// core styles
+import "assets/scss/argon-dashboard-pro-material-ui.scss?v1.0.0";
 
 const App = () => {
   const [item, setItem] = useState({ msg: null, anchorEl: null });
@@ -32,14 +50,27 @@ const App = () => {
               <img src={logo} />
             </div>
           </Typography>
-          <IconButton onClick={handleClick} color="inherit" style={{ marginLeft: "auto", paddingRight: "1vh" }}>
+          <IconButton
+            onClick={handleClick}
+            color="inherit"
+            style={{ marginLeft: "auto", paddingRight: "1vh" }}
+          >
             <Reorder />
           </IconButton>
-          <Menu id="simple-menu" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
             <MenuItem component={Link} to="/home" onClick={handleClose}>
               Home
             </MenuItem>
-            <MenuItem component={Link} to="/accountbenefits" onClick={handleClose}>
+            <MenuItem
+              component={Link}
+              to="/accountbenefits"
+              onClick={handleClose}
+            >
               Account Benefits
             </MenuItem>
             <MenuItem component={Link} to="/tripplanner" onClick={handleClose}>
@@ -53,13 +84,17 @@ const App = () => {
       </AppBar>
       <div>
         <Route exact path="/" render={() => <Redirect to="/home" />} />
-        <Route exact path="/accountbenefits" render={() => <AccountBenefits />} />
+        <Route
+          exact
+          path="/accountbenefits"
+          render={() => <AccountBenefits />}
+        />
         <Route path="/tripplanner" render={() => <TripPlannerComponent />} />
         <Route exact path="/addreview" render={() => <AddReview />} />
         <Route path="/packlist" render={() => <PackListComponent />} />
         <Route path="/home" component={Home} />
       </div>
-    </MuiThemeProvider >
+    </MuiThemeProvider>
   );
 };
 
