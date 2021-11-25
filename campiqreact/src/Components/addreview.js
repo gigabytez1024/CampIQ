96ec1daaab847f996627f1a3dc4e335773fe431d
gigabytez <bradleyvachon@gmail.com>
2021-11-25 17:32:48 -0500

    getting ready to pull

--- campiqreact/src/Components/addreview.js
+++ campiqreact/src/Components/addreview.js
@@ -6,6 +6,7 @@ import {
 } from '@material-ui/core';
 import theme from '../theme';
 import Location from "./location";
+import ReactStars from "react-rating-stars-component";
 
 const AddReview = () => {
     let [campsite, setCampsite] = useState("");
@@ -19,13 +20,56 @@ const AddReview = () => {
 
     return (
         <MuiThemeProvider theme={theme}>
-            <Location campsite={chosenSite} />
+            {
+                !campsiteChosen && (
+                    <Location campsite={chosenSite} />
+                )
+            }
             {
                 campsiteChosen && (
                     <Card>
-                        <CardHeader title={<p>Submit a review for: <b>{campsite}</b></p>} style={{ textAlign: "center" }} />
+                        <CardHeader title={<h1>{campsite}</h1>} style={{ textAlign: "center" }} />
+                        {/* image? */}
                         <div style={{ margin: 20 }}>
-                            <textarea style={{ width: '100%', height: '12vh' }}></textarea>
+                            <div class="row" style={{ marginLeft: 20 }}>
+                                <div style={{ marginRight: 54, marginTop: 20 }}>
+                                    <h4>App Score: </h4>
+                                </div>
+                                <ReactStars
+                                    size={40}
+                                    isHalf={true}
+                                    value={3}   //will query from database
+                                    edit={false}
+                                    activeColor="#ffd700"
+                                />
+                            </div>
+                            <div class="row" style={{ marginLeft: 20 }}>
+                                <div style={{ marginRight: 40, marginTop: 20 }}>
+                                    <h4>Users Score: </h4>
+                                </div>
+                                <ReactStars
+                                    size={40}
+                                    isHalf={true}
+                                    value={2.5} //will query from database
+                                    a11y={true}
+                                    edit={false}
+                                    activeColor="#ffd700"
+                                />
+                            </div>
+
+                            <div style={{ margin: 20, marginBottom: 0 }}>
+                                <h2>Submit a review</h2>
+                                <div class="row" style={{ marginLeft: 0 }}>
+                                    <p>Attribute: </p>
+                                    <ReactStars
+                                        size={20}
+                                        isHalf={true}
+                                        a11y={true}
+                                        activeColor="#ffd700"
+                                    />
+                                </div>
+                                <textarea style={{ width: '100%', height: '10vh' }}></textarea>
+                            </div>
                         </div>
                     </Card>
                 )
